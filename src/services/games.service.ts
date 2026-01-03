ed921970013d3c65697ab3ec06b4b73559e8ae2e
Yehali <yaliby4@gmail.com>
2026-01-04 00:06:48 +0200

    Fix image loading by moving assets to public

--- src/services/games.service.ts
+++ src/services/games.service.ts
@@ -2,7 +2,7 @@
 
 export function getGames(): Game[] {
   return [
-    { id: '1', title: 'ארבע בשורה' ,image: 'img/Screenshot 2026-01-02 152831.png',path: '/game/connect-four',},
-    { id: '2', title: 'דמקה' ,image: 'img/Screenshot 2026-01-02 152420.png',path: '/game/checkers',}
+    { id: '1', title: 'ארבע בשורה' ,image: 'Screenshot 2026-01-02 152831.png',path: '/game/connect-four',},
+    { id: '2', title: 'דמקה' ,image: 'Screenshot 2026-01-02 152420.png',path: '/game/checkers',}
   ];
 }
